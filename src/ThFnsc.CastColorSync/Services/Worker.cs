a435d8c991a894d24f031341dd9c1f78e496fdc8
Thiago Fonseca <thfnsc@outlook.com>
2023-08-23 21:15:32 -0300

    Initial

--- src/ThFnsc.CastColorSync/Services/Worker.cs
+++ src/ThFnsc.CastColorSync/Services/Worker.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Options;
-using ColorHelper;
+using Kevsoft.WLED;
+using System.Text.Json;
 
 namespace ThFnsc.CastColorSync.Services;
 
@@ -8,14 +9,17 @@ public class Worker : BackgroundService
     private string? _lastPicture;
     private readonly IServiceProvider _serviceProvider;
     private readonly IColorPicker _colorPicker;
-    private readonly HashSet<string> _lightsTurnedOn = new();
+    private readonly IOptions<AppSettings> _appSettings;
+    private StateResponse? _previousLightState;
 
     public Worker(
         IServiceProvider serviceProvider,
-        IColorPicker colorPicker)
+        IColorPicker colorPicker,
+        IOptions<AppSettings> appSettings)
     {
         _serviceProvider = serviceProvider;
         _colorPicker = colorPicker;
+        _appSettings = appSettings;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -28,6 +32,7 @@ public class Worker : BackgroundService
                 var service = scope.ServiceProvider;
                 var hassClient = service.GetRequiredService<IHassClient>();
                 var appSettings = service.GetRequiredService<IOptions<AppSettings>>().Value;
+                var wledClient = service.GetRequiredService<WLedClient>();
 
                 var pictureUrls = await Task.WhenAll(appSettings.Hass.SourceDevices.Select(d => PlayingAlbumImageAsync(hassClient, d)));
                 var pictureUrl = pictureUrls.Where(p => p is not null).FirstOrDefault();
@@ -37,17 +42,17 @@ public class Worker : BackgroundService
                     _lastPicture = pictureUrl;
 
                     if (pictureUrl is null)
-                        await TurnLightsBackOffAsync(hassClient);
+                    {
+                        if (_previousLightState is not null)
+                            await SetLightStateAsync(wledClient, _previousLightState);
+                    }
                     else
                     {
-                        var states = await Task.WhenAll(appSettings.Hass.Lights.Select(async light => (light, state: await GetLightStateAsync(hassClient, light))));
+                        _previousLightState = await wledClient.GetState();
 
-                        foreach (var (light, state) in states.Where(t => !t.state))
-                            _lightsTurnedOn.Add(light);
+                        var palette = await GetPaletteFromImageUrlAsync(hassClient, pictureUrl);
 
-                        var color = await GetColorFromImageUrl(hassClient, pictureUrl);
-
-                        await SetLightsColorAsync(hassClient, appSettings.Hass.Lights, color);
+                        await SetLightColorAsync(wledClient, palette);
                     }
                 }
                 try
@@ -59,20 +64,16 @@ public class Worker : BackgroundService
         }
         finally
         {
-            await TurnLightsBackOffAsync(_serviceProvider.GetRequiredService<IHassClient>());
+            if (_previousLightState != null)
+                await SetLightStateAsync(_serviceProvider.GetRequiredService<WLedClient>(), _previousLightState);
         }
     }
 
-    private static Task TurnLightOffAsync(IHassClient hassClient, string entityId) =>
-        hassClient.Service.CallService("light.turn_off", new
-        {
-            entity_id = entityId
-        });
-
-    private async Task TurnLightsBackOffAsync(IHassClient hassClient)
+    private static Task SetLightStateAsync(WLedClient wLedClient, StateResponse state)
     {
-        await Task.WhenAll(_lightsTurnedOn.Select(l => TurnLightOffAsync(hassClient, l)));
-        _lightsTurnedOn.Clear();
+        var asJson = JsonSerializer.SerializeToUtf8Bytes(state);
+        var asRequest = JsonSerializer.Deserialize<StateRequest>(asJson)!;
+        return wLedClient.Post(asRequest);
     }
 
     private static async Task<string?> PlayingAlbumImageAsync(IHassClient hassClient, string entityId)
@@ -83,26 +84,24 @@ public class Worker : BackgroundService
         return null;
     }
 
-    private static async Task<bool> GetLightStateAsync(IHassClient hassClient, string entityId)
-    {
-        var state = await hassClient.States.GetState(entityId);
-        return state.State is "on";
-    }
-
-    private async Task<HSL> GetColorFromImageUrl(IHassClient hassClient, string pictureUrl)
+    private async Task<Rgb24[]> GetPaletteFromImageUrlAsync(IHassClient hassClient, string pictureUrl)
     {
         var imageStream = await hassClient.GetImageAsync(pictureUrl);
-        return await _colorPicker.GetSignatureColorAsync(imageStream);
+        return await _colorPicker.GetColorPalette(imageStream, 3);
     }
 
-    private static Task SetLightsColorAsync(IHassClient hassClient, string[] entityIds, HSL color)
+    private Task SetLightColorAsync(WLedClient wLedClient, Rgb24[] color)
     {
-        var tasks = entityIds.Select(e => hassClient.Service.CallService("light.turn_on", new
-        {
-            entity_id = e,
-            hs_color = new int[] { color.H, color.S < 5 ? 0 : 100 }
-        }));
+        var segment = _appSettings.Value.WLed.SegmentOptions;
+        segment.SegmentState = true;
+        segment.Colors = color.Select(c => new int[] { c.R, c.G, c.B }).ToArray();
 
-        return Task.WhenAll(tasks);
+        var newState = new StateRequest()
+        {
+            On = true,
+            Segments = new SegmentRequest[] { segment }
+        };
+
+        return wLedClient.Post(newState);
     }
 }
